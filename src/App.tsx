fa3eb5e8d01603fec211af915047c66641439aa5
milanrai <milanraiyt07@gmail.com>
2024-07-30 17:39:28 +0545

    bill and total price added

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import { useRemoveItem } from "./customHook/useRemoveItem"
 import { useGetLocalStorage } from "./customHook/useGetLocalStorage"
 import { useSetLocalStorage } from "./customHook/useSetLocalStorage"
 import Chef from "./components/Chef"
+import Bill from "./components/Bill"
 
 const App: React.FC = () => {
   const [spinach, setSpinach] = useState<number>(useGetLocalStorage('spinach'));
   const [onion, setOnion] = useState<number>(useGetLocalStorage('onion'));
   const [steak, setSteak] = useState<number>(useGetLocalStorage('steak'));
   const [tomato, setTomato] = useState<number>(useGetLocalStorage('tomato'));
-  const [message, setMessage] = useState<string>('Add the Recipes as you wish')
+  const [message, setMessage] = useState<string>('Add the Recipes as you wish');
+  const [showPricing, setShowPricing] = useState<boolean>(false)
   console.log(message)
   useEffect(() => {
     useSetLocalStorage({ spinach, onion, steak, tomato })
@@ -20,29 +22,46 @@ const App: React.FC = () => {
   return (
     <>
       <main className="h-screen w-full ImageBackground bg-center relative ">
-        <div className="absolute bottom-[20%] left-1/2 -translate-x-1/2">
-          <Burger
-            spinach={spinach}
-            onion={onion}
-            steak={steak}
-            tomato={tomato}
-          />
-        </div>
-        <div className="ButtonDiv pt-2">
-          <button className="bg-green-700 ItemButton" onClick={() => useAddItem('Spinach', spinach, setSpinach, setMessage)}>Add Spinach</button>
-          <button className="bg-purple-700 ItemButton" onClick={() => useAddItem('Onion', onion, setOnion, setMessage)}>Add Onion</button>
-          <button className="bg-pink-700 ItemButton" onClick={() => useAddItem('Steak', steak, setSteak, setMessage)}>Add Steak</button>
-          <button className="bg-red-700 ItemButton" onClick={() => useAddItem('Tomato', tomato, setTomato, setMessage)}>Add Tomato</button>
-        </div><br></br>
-        <div className="ButtonDiv">
-          <button className="bg-green-900 ItemButton" onClick={() => useRemoveItem('Spinach', spinach, setSpinach, setMessage)}>Remove Spinach</button>
-          <button className="bg-purple-900 ItemButton" onClick={() => useRemoveItem('Onion', onion, setOnion, setMessage)}>Remove Onion</button>
-          <button className="bg-pink-900 ItemButton" onClick={() => useRemoveItem('Steak', steak, setSteak, setMessage)}>Remove Steak</button>
-          <button className="bg-red-900 ItemButton" onClick={() => useRemoveItem('Tomato', tomato, setTomato, setMessage)}>Remove Tomato</button>
-        </div>
-        <div className="absolute right-5 bottom-[30%]">
-          <Chef message={message} />
-        </div>
+        {showPricing ?
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2" >
+            <Bill
+              spinach={spinach}
+              onion={onion}
+              steak={steak}
+              tomato={tomato}
+              setState={setShowPricing}
+            />
+          </div>
+          :
+          <div>
+            <div className="absolute bottom-[20%] left-1/2 -translate-x-1/2">
+              <Burger
+                spinach={spinach}
+                onion={onion}
+                steak={steak}
+                tomato={tomato}
+              />
+            </div>
+            <div className="ButtonDiv pt-2">
+              <button className="bg-red-700 ItemButton" onClick={() => useAddItem('Tomato', tomato, setTomato, setMessage)}>Add Tomato</button>
+              <button className="bg-green-700 ItemButton" onClick={() => useAddItem('Spinach', spinach, setSpinach, setMessage)}>Add Spinach</button>
+              <button className="bg-purple-700 ItemButton" onClick={() => useAddItem('Onion', onion, setOnion, setMessage)}>Add Onion</button>
+              <button className="bg-pink-700 ItemButton" onClick={() => useAddItem('Steak', steak, setSteak, setMessage)}>Add Steak</button>
+            </div><br></br>
+            <div className="ButtonDiv">
+              <button className="bg-red-900 ItemButton" onClick={() => useRemoveItem('Tomato', tomato, setTomato, setMessage)}>Remove Tomato</button>
+              <button className="bg-green-900 ItemButton" onClick={() => useRemoveItem('Spinach', spinach, setSpinach, setMessage)}>Remove Spinach</button>
+              <button className="bg-purple-900 ItemButton" onClick={() => useRemoveItem('Onion', onion, setOnion, setMessage)}>Remove Onion</button>
+              <button className="bg-pink-900 ItemButton" onClick={() => useRemoveItem('Steak', steak, setSteak, setMessage)}>Remove Steak</button>
+            </div>
+            <div className="absolute right-5 bottom-[30%]">
+              <Chef message={message} />
+            </div>
+            <div className="absolute left-5 bottom-[30%]">
+              <button className="PriceButton" onClick={() => setShowPricing(true)}>Burger Pricing</button>
+            </div>
+          </div>
+        }
       </main>
     </>
   )
---
+++ src/components/Bill.tsx
@@ -0,0 +1,106 @@
+import Spinach from '/images/spinach.png';
+import Steak from '/images/steak.png';
+import Onion from '/images/onion.png'
+import Tomato from '/images/tomato.png'
+import React, { useEffect, useState } from 'react';
+
+interface propType {
+    spinach: number,
+    steak: number,
+    onion: number,
+    tomato: number,
+    setState: React.Dispatch<React.SetStateAction<boolean>>
+}
+interface items {
+    spinach: number,
+    steak: number,
+    onion: number,
+    tomato: number,
+}
+const Bill: React.FC<propType> = ({ spinach, steak, onion, tomato, setState }) => {
+    const [totalPrice, setTotalPrice] = useState<number>();
+
+    const rate: items = {
+        spinach: 70,
+        tomato: 50,
+        steak: 200,
+        onion: 50
+    }
+    const number: items = {
+        spinach: spinach,
+        tomato: tomato,
+        steak: steak,
+        onion: onion
+    }
+    useEffect(() => {
+        const total: number = (Object.keys(rate) as (keyof items)[]).reduce((accumulator, item) => {
+            return accumulator + (rate[item] * number[item]);
+        }, 0);
+        setTotalPrice(total);
+    }, [rate, number]);
+    return (
+        <>
+            <section>
+                <h1 className="mb-6 text-center text-xl text-white font-bold">Your Burger Price</h1>
+                <div className="text-white   bg-[rgba(0,0,0,0.7)] rounded">
+                    <div className="BillDiv mb-7 border-b-2 border-white font-semibold">
+                        <div className="BillItem">Items</div>
+                        <div className="BillItem">No. of item</div>
+                        <div className="BillItem">Price</div>
+                    </div>
+                    {tomato !== 0 && (
+                        <div className="BillDiv">
+                            <div className="BillItem">
+                                <img src={Tomato}></img>
+                            </div>
+                            <div className="BillItem">{tomato}</div>
+                            <div className="BillItem">{tomato * rate.tomato}</div>
+                        </div>
+                    )
+                    }
+                    {spinach !== 0 && (
+                        <div className="BillDiv">
+                            <div className="BillItem">
+                                <img src={Spinach}></img>
+                            </div>
+                            <div className="BillItem">{spinach}</div>
+                            <div className="BillItem">{spinach * rate.spinach}</div>
+                        </div>
+                    )
+                    }
+                    {onion !== 0 && (
+                        <div className="BillDiv">
+                            <div className="BillItem">
+                                <img src={Onion}></img>
+                            </div>
+                            <div className="BillItem">{onion}</div>
+                            <div className="BillItem">{onion * rate.onion}</div>
+                        </div>
+                    )
+                    }
+                    {steak !== 0 && (
+                        <div className="BillDiv">
+                            <div className="BillItem">
+                                <img src={Steak}></img>
+                            </div>
+                            <div className="BillItem">{steak}</div>
+                            <div className="BillItem">{steak * rate.steak}</div>
+                        </div>
+                    )
+                    }
+                    <div className='BillDiv font-semibold'>
+                        <h2 className='BillItem grow'>Total</h2>
+                        <p className='BillItem underline'>{totalPrice}</p>
+                    </div>
+                </div>
+                <div className='text-center'>
+                    <button className='PriceButton' onClick={() => setState(false)}>
+                        Go back to kitchen
+                    </button>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Bill
